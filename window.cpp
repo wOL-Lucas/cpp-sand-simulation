d8b2e298261feb8a7147c343b38006227d094dd1
Lucas <lucasefernandes333@gmail.com>
2025-01-05 18:00:55 -0300

    feat: Add sand physics
    This commit implements the sand initial physics and behavior.
    
    - Refactor window pixels to grid layout

--- window.cpp
+++ window.cpp
@@ -1,28 +1,128 @@
 #include <windows.h>
 #include <winuser.h>
+#include <windowsx.h>
+#include <vector>
+
+
+const int sandSize = 10;
+int gridWidth;
+int gridHeight;
+
+std::vector<std::vector<bool>> grid;
+
+
+void initializeGrid(int width, int height) {
+  gridWidth = width / sandSize;
+  gridHeight = height / sandSize;
+  grid.resize(gridHeight, std::vector<bool>(gridWidth, false));
+}
+
+void updateGrid() {
+  for (int y = gridHeight - 1; y >= 0; y--) {
+    for (int x = 0; x < gridWidth; x++) {
+      
+      if (grid[y][x] && y + 1 < gridHeight) {
+        if (!grid[y + 1][x]) {
+          grid[y + 1][x] = true;
+          grid[y][x] = false;
+        }
+
+        else if(y + 2 < gridHeight && grid[y + 2][x]){
+          if(x + 1 < gridWidth) {
+            if(!grid[y + 1][x + 1]) {
+              grid[y][x] = false;
+              grid[y + 1][x + 1] = true;
+            }
+          } else if (x - 1 < gridWidth){
+              grid[y][x] = false;
+              grid[y + 1][x - 1] = true;
+          }
+        }
+      }
+    }
+  }
+}
+
+int createSand(HWND hwnd, LPARAM lParam){
+  int x = GET_X_LPARAM(lParam);
+  int y = GET_Y_LPARAM(lParam);
+
+  int gridX = x / sandSize;
+  int gridY = y / sandSize;
+
+  
+  if (gridX < 0 || gridX >= gridWidth || gridY < 0 || gridY >= gridHeight) {
+    return 0;
+  }
+
+  grid[gridY][gridX] = true;
+  updateGrid();
+
+  InvalidateRect(hwnd, NULL, FALSE);
+
+}
 
 LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+    updateGrid();
     switch (uMsg)
     {
+
+    case WM_CREATE:
+    {
+        SetTimer(hwnd, 100, 1, NULL);
+        RECT rect;
+        GetClientRect(hwnd, &rect);
+        initializeGrid(rect.right, rect.bottom);
+        return 0;
+    }
+
+    case WM_TIMER:
+    {
+        updateGrid();
+        InvalidateRect(hwnd, NULL, FALSE);
+        return 0;
+    }
+
+    case WM_LBUTTONDOWN:
+    {
+        createSand(hwnd, lParam);
+        return 0;
+    }
+
+    case WM_MOUSEMOVE:
+    {
+        if(wParam & MK_LBUTTON) {
+          createSand(hwnd, lParam);
+        }
+
+        return 0;
+    }
 
     case WM_PAINT:
     {
         //This init the drawing context...
         PAINTSTRUCT ps;
         HDC hdc = BeginPaint(hwnd, &ps);
-
-        RECT rect;
-        GetClientRect(hwnd, &rect);
-        int width = rect.right;
-        int height = rect.bottom;
-
-        int sandSize = 10;
-        for (int y = 0; y < height; y += sandSize) {
-          for (int x = 0; x < width; x += sandSize) {
-
-            // sandSize X sandSize square
-            Rectangle(hdc, x, y, x + sandSize, y + sandSize);
+        
+        for (int y = 0; y < gridHeight; y++) {
+          for (int x = 0; x < gridWidth; x++) {
+            if (grid[y][x]) {
+              RECT rect;
+              rect.left = x * sandSize;
+              rect.top = y * sandSize;
+              rect.right = rect.left + sandSize;
+              rect.bottom = rect.top + sandSize;
+              FillRect(hdc, &rect, (HBRUSH)GetStockObject(BLACK_BRUSH));
+            }
+            else {
+              RECT rect;
+              rect.left = x * sandSize;
+              rect.top = y * sandSize;
+              rect.right = rect.left + sandSize;
+              rect.bottom = rect.top + sandSize;
+              FillRect(hdc, &rect, (HBRUSH)GetStockObject(WHITE_BRUSH));
+            }
           }
         }
 
